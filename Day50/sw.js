7a8d653f35f99780aee3d11a684c93aceede023d
Harshil Chovatiya <131672641+harshilchovatiya@users.noreply.github.com>
2023-12-21 16:58:26 +0530

    Update sw.js

--- Day50/sw.js
+++ Day50/sw.js
@@ -14,8 +14,26 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
-});
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => response || fetch(event.request))
+        .catch((error) => {
+          console.error('Fetch error:', error);
+        })
+    );
+  });
+
+  self.addEventListener('activate', (event) => {
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+  
